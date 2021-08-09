cf45ed9d588a92ba7a225dd82deabae7aa50f565
Abi Gibbs <78177090+Abi-gibbs@users.noreply.github.com>
2021-08-09 22:30:14 +0000

    Working with the list .

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-// card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('Welcome to my card game!')
+// player1Name = prompt ('what is player ones name?')
+// player2Name = prompt ('What is player twos name?')
 
-let card = Math.floor(Math.random() * 13 + 1 )
 
 
-// drawCard = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+cardList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-let drawCard = Math.floor(Math.random() * 13 + 1 )
+let card = Math.floor(Math.random() * cardList.length )
+
+card1=cardList[card]
+
+drawCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+let draw = Math.floor(Math.random() * drawCards.length )
+
+drawCard1 = drawCards[draw]
 
 
 var guess = prompt ('Guess high or low')
-if ( guess ==  'high' || 'h' && drawCard > card ) {
+if ( guess ==  'high' || 'h'  && drawCard1 > card1 ) {
   console.log('You got it right')
 }
-else if ( guess == 'low' || 'l' && drawCard < card){
+else if ( guess == 'low' || 'l' && drawCard1 < card1){
   console.log('You got it right')
 }
 else {
   console.log ('You got it wrong!')
 }
 
-// else {
-//   console.log('Same card')
-// }
+
 
