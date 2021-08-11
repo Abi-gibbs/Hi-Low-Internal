5010f72d2aae4d9f2304ed68ee30f129a598a81d
Abi Gibbs <78177090+Abi-gibbs@users.noreply.github.com>
2021-08-11 02:23:10 +0000

    Commit before adding functions

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
-// console.log('Welcome to my card game!')
-// player1Name = prompt ('what is player ones name?')
-// player2Name = prompt ('What is player twos name?')
+let round = prompt('How many rounds do you want to play?');
+
+let roundNum = 0;
+
+	
+let cardList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+let pickedCard = Math.floor(Math.random() * cardList.length);
+
+let card1 = cardList[pickedCard];
+console.log(card1)
 
 
 
-cardList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-let card = Math.floor(Math.random() * cardList.length )
-
-card1=cardList[card]
-
-drawCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-let draw = Math.floor(Math.random() * drawCards.length )
-
-drawCard1 = drawCards[draw]
 
 
-var guess = prompt ('Guess high or low')
-if ( guess ==  'high' || 'h'  && drawCard1 > card1 ) {
-  console.log('You got it right')
-}
-else if ( guess == 'low' || 'l' && drawCard1 < card1){
-  console.log('You got it right')
-}
-else {
-  console.log ('You got it wrong!')
-}
+let drawCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+let draw = Math.floor(Math.random() * drawCards.length);
+
+let drawCard1 = drawCards[draw];
+console.log(drawCard1)
+
+do {
+
+	let guess1 = prompt('Guess high or low')
+
+	if (guess1 == 'high' || 'h' && drawCard1 < card1){
+		console.log('You got it right')
+
+	} else if (guess == 'low' || 'l' && drawCard1 < card1){
+    console.log('You got it right')
+
+	} else {
+		console.log('You got it wrong!')
+	}
+  
+  roundNum ++ 
+
+} while (roundNum < round);
+
 
 
 
