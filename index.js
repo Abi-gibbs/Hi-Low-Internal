fd4e626db9e80490d6456ebbe87c04c8d8d5f57e
Abi Gibbs <78177090+Abi-gibbs@users.noreply.github.com>
2021-08-22 05:51:42 +0000

    rounds work but always goes to higherguess function

--- index.js
+++ index.js
@@ -2,56 +2,64 @@ let round = prompt('How many rounds do you want to play?');
 
 let roundNum = 0;
 
-	
+
 let cardList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-
-let pickedCard = Math.floor(Math.random() * cardList.length);
-
-let card1 = cardList[pickedCard];
-console.log(card1)
-
-
-
-
-
 let drawCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-
-let draw = Math.floor(Math.random() * drawCards.length);
-
-let drawCard1 = drawCards[draw];
-console.log(drawCard1)
 
 do {
 
-	let guess1 = prompt('Guess high or low')
+  //pick the first card
+  let pickedCard = Math.floor(Math.random() * cardList.length);
+  let card1 = cardList[pickedCard];
 
-	if (guess1 == 'high' || 'h'){
-    higherGuess()
+  //tell the user what was drawn and ask for their guess
+  console.log('The first card drawn is ' + card1);
+  let guess1 = prompt('Guess wether the next card drawn is going to be higher or lower');
+
+  //pick the second card
+  let draw = Math.floor(Math.random() * drawCards.length);
+  let cardDrawn = drawCards[draw];
+
+  //check if higher or lower
+  console.log(guess1)
+  if (guess1 == 'high' || 'h' || 'higher') {
+    higherGuess(cardDrawn, card1);
+
+  } else if (guess1 == 'low' || 'l' || 'lower') {
+    lowerGuess(cardDrawn, card1);
+
+  } else {
+
+    console.log('invalid answer please try again')
+    let guess1 = prompt('Guess high or low')
+
   }
-  
-	} else if (guess == 'low' || 'l' && drawCard1 < card1){
-    console.log('You got it right')
 
-	} else {
-		console.log('You got it wrong!')
-	}
-  
-  roundNum ++ 
-
+  roundNum++
+  // higherGuess();
 } while (roundNum < round);
 
-
-higherGuess();
-function higherGuess(){
-if (drawCard1 < card1){
-  console.log('Correct!')
+function higherGuess(cardDrawn, card1) {
+  console.log('higher:');
+  if (cardDrawn > card1) {
+    console.log('Correct!');
+    console.log('The card was ' + cardDrawn)
+  }
+  else {
+    console.log('incorrect!')
+    console.log('The card was ' + cardDrawn)
+  }
 }
-else {
-  console.log('incorrect!')
+
+ function lowerGuess(cardDrawn, card1) {
+   console.log(cardDrawn + ', ' + card1);
+
+  if (cardDrawn < card1) {
+    console.log('Correct!')
+    console.log('The card was ' + cardDrawn)
+  }
+  else {
+    console.log('Incorrect!')
+    console.log('The card was ' + cardDrawn)
+  }
 }
-console.log('The card was ' + drawCard1)
-}
-
-
-
-
